ce7175e120f40a1901e9638c1b8c2b602154713e
AKINTILO Steeven <steevenakintilo>
2021-07-21 01:36:34 +0200

    80 % of the winning condition

--- connect4.py
+++ connect4.py
@@ -33,6 +33,9 @@ j = 0
 for i in range(42):
     board.append(" ")
 empty_list = [" "," "," "," "," "," "]
+p1 = ["X","X","X","X"]
+p2win_list = ["O","O","O","O"]
+#p1win = [board[35],board[36],board[37],board[38]]
 b1 =  [board[35],board[28],board[21],board[14],board[7],board[0]]
 b2 =  [board[36],board[29],board[22],board[15],board[8],board[1]]
 b3 =  [board[37],board[30],board[23],board[16],board[9],board[2]]
@@ -42,6 +45,7 @@ b6 =  [board[40],board[33],board[26],board[19],board[12],board[5]]
 b7 =  [board[41],board[34],board[27],board[20],board[13],board[6]]
 print(b1[0])
 i = 0
+idx = 0
 check1 = 0
 check2 = 0
 check3 = 0
@@ -51,6 +55,7 @@ check6 = 0
 check7 = 0
 print_board(board)
 while True:
+    #p1win = [board[35],board[36],board[37],board[38]]
     #print_board(board)
     val = input("J1 choisi une colonne: ")
     if val == "1":
@@ -88,6 +93,65 @@ while True:
             if b7 == empty_list:
                 board[41 - check7] = "X"
         check7 = check7 + 7
+    w = [board[35],board[36],board[37],board[38]]
+    w2 = [board[36],board[37],board[38],board[39]]
+    w3 = [board[37],board[38],board[39],board[40]]
+    w4 = [board[38],board[39],board[40],board[41]]
+    w5 = [board[28],board[29],board[30],board[31]]
+    w6 = [board[29],board[30],board[31],board[32]]
+    w7 = [board[30],board[31],board[32],board[33]]
+    w8 = [board[31],board[32],board[33],board[34]]
+    w9 = [board[21],board[22],board[23],board[24]]
+    w10 = [board[22],board[23],board[24],board[25]]
+    w11 = [board[23],board[24],board[25],board[26]]
+    w12 = [board[24],board[25],board[26],board[27]]
+    w13 = [board[14],board[15],board[16],board[17]]
+    w14 = [board[15],board[16],board[17],board[18]]
+    w15 = [board[16],board[17],board[18],board[19]]
+    w16 = [board[17],board[18],board[19],board[20]]
+    w17 = [board[7],board[8],board[9],board[10]]
+    w18 = [board[8],board[9],board[10],board[11]]
+    w19 = [board[9],board[10],board[11],board[12]]
+    w20 = [board[10],board[11],board[12],board[13]]
+    w21 = [board[35],board[28],board[21],board[14]]
+    w22 = [board[36],board[29],board[22],board[15]]
+    w23 = [board[37],board[30],board[23],board[16]]
+    w24 = [board[38],board[31],board[24],board[17]]
+    w25 = [board[28],board[21],board[14],board[7]]
+    w26 = [board[29],board[22],board[15],board[8]]
+    w27 = [board[30],board[23],board[16],board[9]]
+    w28 = [board[31],board[24],board[17],board[10]]
+    w29 = [board[21],board[14],board[7],board[0]]
+    w30 = [board[22],board[15],board[8],board[1]]
+    w31 = [board[23],board[16],board[9],board[2]]
+    w32 = [board[24],board[17],board[10],board[3]]
+    w33 = [board[39],board[32],board[25],board[18]]
+    w34 = [board[40],board[33],board[26],board[19]]
+    w35 = [board[41],board[34],board[27],board[20]]
+    w36 = [board[32],board[25],board[18],board[11]]
+    w37 = [board[33],board[26],board[19],board[12]]
+    w38 = [board[34],board[27],board[20],board[13]]
+    w39 = [board[25],board[18],board[11],board[4]]
+    w40 = [board[26],board[19],board[12],board[5]]
+    w41 = [board[27],board[20],board[13],board[6]]
+    w42 = [board[35],board[29],board[23],board[17]]
+    w43 = [board[29],board[23],board[17],board[11]]
+    w44 = [board[23],board[17],board[11],board[4]]
+    w45 = [board[36],board[30],board[24],board[18]]
+    w46 = [board[30],board[24],board[18],board[12]]
+    w47 = [board[24],board[18],board[12],board[6]]
+    w48 = [board[37],board[31],board[25],board[19]]
+    w49 = [board[31],board[25],board[19],board[13]]
+    w50 = [board[38],board[32],board[26],board[20]]
+    w51 = [board[28],board[22],board[16],board[10]]
+    w52 = [board[22],board[16],board[10],board[4]]
+    w53 = [board[21],board[15],board[9],board[3]]
+    
+    #print(w33)
+    if w == p1 or w2 == p1 or w3 == p1 or w4 == p1 or w5 == p1 or w6 == p1 or w7 == p1 or w8 == p1 or w9 == p1 or w10 == p1 or w11 == p1 or w12 == p1 or w13 == p1 or w14 == p1 or w15 == p1 or w16 == p1 or w17 == p1 or w18 == p1 or w19 == p1 or w20 == p1 or w21 == p1 or w22 == p1  or w23 == p1 or w24 == p1 or w25 == p1 or w26 == p1 or w27 == p1 or w28 == p1 or w29 == p1 or w30 == p1 or w31 == p1 or w32 == p1 or w33 == p1 or w34 == p1 or w35 == p1 or w36 == p1 or w37 == p1 or w38 == p1 or w39 == p1 or w40 == p1 or w41 == p1 or w42 == p1 or w43 == p1 or w44 == p1 or w45 == p1 or w46 == p1 or w47 == p1 or w48 == p1 or w49 == p1 or w50 == p1 or w51 == p1 or w52 == p1 or w53 == p1:
+        print_board(board)
+        print("You win")
+        quit() 
     print_board(board)
     val2 = input("J2 choisi une colonne: ")
     if val2 == "1":
